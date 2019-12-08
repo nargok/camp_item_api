497048d4a894c5b7e2e4b34131fea1559ba82d53
nargok <nargok@gmail.com>
2019-12-08 16:00:45 +0900

    item show action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,23 @@
 module Api
   module V1
     class ItemsController < ApplicationController
+      before_action :set_item, only: [:show]
 
       def index
         items = Item.order(created_at: :desc)
         render json: { status: 'success', message: 'Loaded items', data: items }
       end
+
+      def show
+        render json: { status: 'success', message: 'Loaded items', data: @item }
+      end
+
+      private
+
+      def  set_item
+        @item = Item.find(params[:id])
+      end
+
     end
   end
 end
