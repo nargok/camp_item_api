39b40df27c74751ac6c2dee412f205ced4e6fbea
nargok <nargok@gmail.com>
2020-01-13 19:56:57 +0900

    change cors settings

--- config/application.rb
+++ config/application.rb
@@ -34,13 +34,6 @@ module CampItems
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins "*"
-        resource "*",
-          headers: :any,
-          methods: [:get, :post, :options, :head]
-      end
-    end
+    config.x.cors_allowed_origins = ENV.fetch('CORS_ALLOWED_ORIGINS', 'http://localhost:8080')
   end
 end
